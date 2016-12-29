d2b3ecf6fece6e89a2d166b9dce99d79a3cf00a8
kshubhu <shubhamkondal007@gmail.com>
2016-12-29 15:00:07 +0530

    first commit

---
+++ abc.js
@@ -0,0 +1,26 @@
+var number=12;
+console.log(number);
+ 
+function abc()
+{var number2= 45;
+console.log(number2);
+}
+abc();
+console.log(number);
+console.log(number2);
+
+
+
+
+var string="hello "
+console.log(string);
+ 
+function one()
+{
+var string2= "world";
+console.log(string2);
+}
+
+one();
+console.log(string);
+
